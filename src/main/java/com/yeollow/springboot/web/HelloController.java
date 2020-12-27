e4dd69cda2437e9da57fa6da3a8e8bacc67bcbac
unknown <ljy2784437@gmail.com>
2020-12-27 17:40:28 +0900

    delete testcode

--- src/main/java/com/yeollow/springboot/web/HelloController.java
+++ src/main/java/com/yeollow/springboot/web/HelloController.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 //    @GetMapping - 해당 URI에 대한 HTTP Method GET의 요청을 받을 수 있는 API를 만들어 줌
 //    @RequestMapping(method = RequestMethod.GET)과 같음
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello Spring!";
-    }
+    @GetMapping("/")
+    public String hello() { return "hello Spring!"; }
 
     @GetMapping("/hello/dto")
 //    @RequestParam - 외부에서 API로 넘긴 parameter를 가져오는 Annotation. name이란 이름으로 외부에서 넘긴 parameter를 String name에 저장하겠다는 의미.
